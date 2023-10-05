2c408d8d66b348bef713a329933ca57b964b6602
naman-negi <85834130+naman-negi@users.noreply.github.com>
2023-10-06 03:24:04 +0530

    commit
    commit

--- src/main/java/com/admin/Adminsearch.java
+++ src/main/java/com/admin/Adminsearch.java
@@ -7,23 +7,36 @@ import javax.servlet.http.*;
 import javax.servlet.RequestDispatcher;
 public class Adminsearch extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private  static  int memoryFlag=0;
     public Adminsearch() {
         super();
     }
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out=response.getWriter();
-		String str1=request.getParameter("username");
-		String str2=request.getParameter("password");
-		RequestDispatcher rd=null;
-		if (str1.equalsIgnoreCase("admin")&&str2.equals("12345")) {
-			response.sendRedirect(request.getContextPath() + "/homepage3.jsp");
-		}else {
-			rd=request.getRequestDispatcher("homepage2.html");
-			out=response.getWriter();
-			rd.include(request, response);
-			out.println("<center><span style='color:red'>Invalid Credentials</span></center>");
-		/*	String str=config.getServletName();//Working of ServletName method of ServletConfig 
-			out.print("</br>"+str); */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		int charsToAdd = 5_000_000;
+		String str1 = request.getParameter("username");
+		if(str1.equalsIgnoreCase("stop"))
+		{
+			memoryFlag = 1;
+		}
+		else
+		{
+			memoryFlag=0;
+		}
+		while (true)
+		{
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < 10; i++)
+			{
+				for (int j = 0; j < charsToAdd; j++)
+				{
+					sb.append('a');
+				}
+			}
+			if (memoryFlag == 1) {
+				sb = null;
+				break;
+			}
 		}
 		}
 
